678d565d44ce6d3d69166a208fa6cee3537f1d1c
tcgriffith <ztc.ustcer@gmail.com>
2017-10-26 16:01:59 +1000

    modify sh script

--- bin/scrapebili.sh
+++ bin/scrapebili.sh
@@ -1,4 +1,11 @@
+
+echo "############################################"
+echo "...begin"
 cd ~/GIT/owaraisite/
 git pull
 
 Rscript ~/GIT/lhdata/R/daily_update.R
+
+git status
+echo "...end"
+echo "############################################"
