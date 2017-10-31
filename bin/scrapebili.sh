fc3dae129eaaec1e67c140d23092e6c529cb2008
tcgriffith <ztc.ustcer@gmail.com>
2017-10-31 22:04:30 +1000

    auto git

--- bin/scrapebili.sh
+++ bin/scrapebili.sh
@@ -1,3 +1,6 @@
+# crontab run everyday at 17:00
+# webscraping bilibili to generate new posts for owaraisite
+# logs: run ~/bin/bilog
 
 echo "############################################"
 echo "...begin"
@@ -7,5 +10,10 @@ git pull
 Rscript ~/GIT/lhdata/R/daily_update.R
 
 git status
+
+git add .
+git commit -m "daily scratch"
+git push
+
 echo "...end"
 echo "############################################"
