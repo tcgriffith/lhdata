b0571f712280846b63e56e3e44358c288bd46e35
tcgriffith <ztc.ustcer@gmail.com>
2017-11-17 14:34:49 +1000

    wordcloud doesn't need to run so frequently

--- R/daily_update.R
+++ R/daily_update.R
@@ -4,6 +4,6 @@ runAllChunks <- function(rmd, envir=globalenv()){
   knitr::purl(rmd, output=tempR)
   sys.source(tempR, envir=envir)
 }
-runAllChunks("~/GIT/lhdata/notebook/wordcloud_data.Rmd")
+#runAllChunks("~/GIT/lhdata/notebook/wordcloud_data.Rmd")
 runAllChunks("~/GIT/lhdata/notebook/daily_updates.Rmd")
 runAllChunks("~/GIT/lhdata/notebook/update_tags.Rmd")
