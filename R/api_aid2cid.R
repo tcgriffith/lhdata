ed217f2ead65410faf304d4ab9200ddb126d2123
tcgriffith <ztc.ustcer@gmail.com>
2018-06-22 11:06:17 +1000

    aaaand this

--- R/api_aid2cid.R
+++ R/api_aid2cid.R
@@ -12,7 +12,9 @@
 #' api_aid2cid(2441580)
 api_aid2cid <-function(aid){
   url2 = paste0("http://www.bilibili.com/widget/getPageList?aid=",aid)
-  req <- curl::curl_fetch_memory(url2)
+  h <- curl::new_handle(useragent = paste("jsonlite /",
+                                          R.version.string), ssl_verifypeer=FALSE)
+  req <- curl::curl_fetch_memory(url2, handler=h)
   if (req$status_code==200){
     test2 <- fromJSON_fix(url2)
     return(test2$cid)
