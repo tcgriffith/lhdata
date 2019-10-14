b64d0cfd28fd08319d1de132922d961becc03154
tcgriffith <ztc.ustcer@gmail.com>
2019-10-14 11:47:29 +1000

    refactor

--- bin/scrapebili.sh
+++ bin/scrapebili.sh
@@ -10,13 +10,11 @@ git pull
 # Rscript ~/GIT/lhdata/R/daily_update.R
 
 ~/GIT/lhdata/bin/update_wiki.sh
-
 ~/GIT/lhdata/R/run_rmd.R  ~/GIT/lhdata/notebook/update_jarutower.Rmd
 ~/GIT/lhdata/R/run_rmd.R  ~/GIT/lhdata/notebook/daily_updates.Rmd
 ~/GIT/lhdata/R/run_rmd.R  ~/GIT/lhdata/notebook/update_from_flarum.Rmd
-~/GIT/lhdata/R/run_rmd.R  ~/GIT/lhdata/notebook/update_tags.Rmd
-
-~/GIT/lhdata/R/run_rmd.R  ~/GIT/lhdata/notebook/update_bangumi.Rmd
+# ~/GIT/lhdata/R/run_rmd.R  ~/GIT/lhdata/notebook/update_tags.Rmd
+# ~/GIT/lhdata/R/run_rmd.R  ~/GIT/lhdata/notebook/update_bangumi.Rmd
 
 # update meta
 grep -l "bangumi.*段子" ~/GIT/owaraisite/content/post/*.md |xargs mv -t ~/GIT/owaraisite/content/neta/
--- bin/scrapebili_aspen.sh
+++ bin/scrapebili_aspen.sh
@@ -8,35 +8,9 @@
 # webscraping bilibili to generate new posts for owaraisite
 # logs:  ~/logs/
 
-echo "############################################"
-echo "...begin"
 module load R/3.4.0
-cd ~/GIT/owaraisite/
-git pull
 
-~/GIT/lhdata/bin/update_wiki.sh
 
-# Rscript ~/GIT/lhdata/R/daily_update.R ~/GIT/lhdata/notebook/update_jarutower.Rmd
+. ~/GIT/lhdata/bin/scrapebili.sh
 
-~/GIT/lhdata/R/run_rmd.R  ~/GIT/lhdata/notebook/update_jarutower.Rmd
-~/GIT/lhdata/R/run_rmd.R  ~/GIT/lhdata/notebook/daily_updates.Rmd
-~/GIT/lhdata/R/run_rmd.R  ~/GIT/lhdata/notebook/update_from_flarum.Rmd
-~/GIT/lhdata/R/run_rmd.R  ~/GIT/lhdata/notebook/update_tags.Rmd
 
-~/GIT/lhdata/R/run_rmd.R  ~/GIT/lhdata/notebook/update_bangumi.Rmd
-
-# update meta
-grep -l "bangumi.*段子" ~/GIT/owaraisite/content/post/*.md |xargs mv -t ~/GIT/owaraisite/content/neta/
-~/GIT/lhdata/R/run_rmd.R  ~/GIT/lhdata/notebook/update_neta.Rmd
-
-# update flarum
-curl "https://d.owaraiclub.com/api/discussions?filter[q]=tag:whatsnew" > ~/GIT/owaraisite/data/flarum.json
-
-git status
-
-git add .
-git commit -m "daily scratch"
-git push
-
-echo "...end"
-echo "############################################"
