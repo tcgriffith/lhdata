e1d16b918b65b763176a763f0a0faa84a7aa7c65
tcgriffith <ztc.ustcer@gmail.com>
2019-10-14 17:11:29 +1000

    A fix I believe, not like I tested or anything

--- bin/update_wiki.sh
+++ bin/update_wiki.sh
@@ -6,6 +6,10 @@ mkdir -p /tmp/_mywiki
 
 git clone https://github.com/tcgriffith/owarai_wiki.wiki.git /tmp/_mywiki
 
+
+cd /tmp/_mywiki/
+git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit > _Changes.md
+
 cp /tmp/_mywiki/*.md $wikipath
 
 cd $wikipath
