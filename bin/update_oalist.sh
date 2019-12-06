a70e7fc6159667e6968f87df9460a7cadaf688c8
tcgriffith <ztc.ustcer@gmail.com>
2019-12-06 22:13:28 +1000

    update oalist

--- bin/update_oalist.sh
+++ bin/update_oalist.sh
@@ -2,5 +2,24 @@
 cd ~/GIT/lhdata
 
 ~/GIT/lhdata/R/run_rmd.R  ~/GIT/lhdata/notebook/scrape_lh_update.Rmd
-
 ~/GIT/lhdata/R/run_rmd.R  ~/GIT/lhdata/notebook/scrape_ametalk_update.Rmd
+
+
+git pull
+
+git status
+git add .
+git commit -m "update oalist"
+git push
+
+
+cd ~/GIT/owaraisite/
+
+Rscript R/R/buildrmd.R
+
+# git status
+
+git add .
+git commit -m "update oalist"
+
+
